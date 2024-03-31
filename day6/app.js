95ce8b24e30a7056f26ce24c6f74b87f3e71ed1f
Akash Rautela <39892973+akashrautela@users.noreply.github.com>
2024-03-31 16:37:36 +0000

    comparision check

---
+++ day6/app.js
@@ -0,0 +1,13 @@
+//comparision
+
+console.log("2" > 1);
+console.log("0" > 1);
+
+console.log(null > undefined);
+
+
+//strict check ===
+console.log("2" == 2); //true
+console.log("2" === 2); //strict check - false value same but datatype diff
+
+
