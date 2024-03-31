d460cad2641143515999e8c3a5235ffdbeb3ae81
Akash Rautela <39892973+akashrautela@users.noreply.github.com>
2024-03-31 16:32:33 +0000

    operations

---
+++ day5/script.js
@@ -0,0 +1,24 @@
+// operations
+
+let a = 3;
+let negValue = -a;
+console.log(negValue);
+
+let s1 = "akash "
+let s2 = "rautela"
+let s3 = s1+s2;
+console.log(s3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");  // conversion happens based on defined algorithms
+
+console.log(+true);
+
+
+let a1,a2,a3;
+
+a1=a2=a2=4;
+
+
