a78203a7b32611c07bbd46fd0c199498f30669ce
Akash Rautela <39892973+akashrautela@users.noreply.github.com>
2024-03-31 16:16:19 +0000

    variables and datatypes

---
+++ day2/test.js
@@ -0,0 +1,14 @@
+const accountId = 144322 // good practise
+let accountEmail; //= "akashrautela@gmail.com" //good practise
+var accountPassword;//= "123421" // obselete should not be used nowdays
+accountCity = "xyz" // worst practise
+
+// accountId = 2 --> const cannot be changed / not allowed
+//accountEmail = "ar@gmail.com"
+//accountPassword = "12121"
+accountCity = "abc"
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
+
---
+++ day3/script.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code aas newer version
+
+//alert('hello') //ReferenceError: alert is not defined
+
+//datatypes
+//---- primitive data types
+// number --> 2 power 53
+// bigint
+// string --> ""
+// boolean
+// null  --> is a type, a standalone value
+// undefined  --> variable whose value is not defined/assigned
+// symbol  --> uniqueness
+
+
+// object
+console.log(typeof null);  //--> object
+
+console.log(typeof undefined); // --> undefined
+
