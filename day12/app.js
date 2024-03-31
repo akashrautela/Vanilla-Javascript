ca45db67b680f131c8908d15d91d6d88453a5709
Akash Rautela <39892973+akashrautela@users.noreply.github.com>
2024-03-31 17:47:11 +0000

    array basics

---
+++ day12/app.js
@@ -0,0 +1,14 @@
+//ARRAYs in JS
+const arr = [1,2,3,4,5,true,'akash',2.5];
+
+
+//Array methods
+arr.push('rautela');
+// console.log(arr);
+
+// const newStringFromArr = arr.join()
+// console.log(newStringFromArr);
+
+//slice and splice
+// slice - doesnot change original array
+// splice - changes original array also
